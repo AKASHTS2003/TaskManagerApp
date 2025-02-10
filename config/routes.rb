ed43423f782fa988f24e97c1d0682371607d8509
bravestudio <bravestudio@bravestudios-MacBook-Pro.local>
2025-02-09 19:45:07 -0800

    added authentication files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :tasks
-end
+  devise_for :users
 
+  # Define the route for the tasks index page (viewing tasks)
+  resources :tasks
+
+  root to: "tasks#index"  # Redirect to the tasks index page after login
+end
